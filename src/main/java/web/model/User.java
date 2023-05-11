4eef25c5b435a5e584ea6117c05c742e2490514b
Arr <Arromaev@gmail.com>
2023-05-11 12:10:56 +0500

    raw master unloading

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,59 @@
+package web.model;
+
+import javax.persistence.*;
+
+public class User {
+
+    private long id;
+    private String name;
+    private int age;
+    private String lastName;
+
+    public User(String name, int age, String lastName) {
+        this.name = name;
+        this.age = age;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
